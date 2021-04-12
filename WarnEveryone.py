54662a6ee54745b9d06c66ce0ef6a6e645a16f96
Robert <robirtduncan@gmail.com>
2021-04-11 19:18:28 -0700

    added restart fix

--- WarnEveryone.py
+++ WarnEveryone.py
@@ -1,22 +1,26 @@
 
 # Twitterbot/bots/followfollowers.py
 
-import tweepy as tp
+
 from config import create_api
-import csv
 import time
 api = create_api()
 
 def warn_everyone(screen_name):
     oldtweet = ""
+    HelpfulBotSays = " Disclaimer: This post not financial advice. Subsequent and potentially related market swings may be highly volatile. Independent due diligence is always recommended."
     while True:
         tweet = api.user_timeline(screen_name=screen_name, count=1) #get most recent status/tweet
         if tweet == oldtweet:   #if it's the same as the most recent tweet, skip
             time.sleep(60)
             print(tweet[0].id)
             pass
-        else:
-            api.update_status("@" + screen_name + " Disclaimer: This post not financial advice. Subsequent and potentially related market swings may be highly volatile. Independent due diligence is always recommended.", tweet[0].id)
+        elif HelpfulBotSays == api.user_timeline(screen_name="NiceHelpfulBot1", count=1).text:
+            time.sleep(60)
+            print(tweet[0].id)
+            oldtweet = tweet
+            pass
+            api.update_status("@" + screen_name + HelpfulBotSays, tweet[0].id)
             print(tweet[0].id)  # reply to most recent tweet
             oldtweet = tweet
             time.sleep(60)
