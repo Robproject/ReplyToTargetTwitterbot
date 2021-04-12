b484e998b38a1b009193cec9f0ed7f5c47f1c96d
Robert <robirtduncan@gmail.com>
2021-04-11 20:32:32 -0700

    changed matching text to matchin id with replyto for recent status

--- WarnEveryone.py
+++ WarnEveryone.py
@@ -10,18 +10,19 @@ api = create_api()
 def warn_everyone(screen_name):
     oldtweet = ""
     helpful_bot_says = " Disclaimer: This post not financial advice. Subsequent and potentially related market swings may be highly volatile. Independent due diligence is always recommended."
+    make_tweet = "@" + screen_name + helpful_bot_says
     while True:
         tweet = api.user_timeline(screen_name=screen_name, count=1)  # get most recent status/tweet
         if tweet == oldtweet:  # if it's the same as the most recent tweet, skip
             time.sleep(60)
             print(tweet[0].id)
-        elif helpful_bot_says == api.user_timeline(screen_name="NiceHelpfulBot1", count=1)[0].text:
+        elif tweet[0].id == api.user_timeline(screen_name="NiceHelpfulBot1", count=1)[0].in_reply_to_status_id:
             # see bot's most recent tweet text, if its the same then sleep
             time.sleep(60)
             print(tweet[0].id)
             oldtweet = tweet
         else:
-            api.update_status("@" + screen_name + helpful_bot_says, tweet[0].id)
+            api.update_status(make_tweet, tweet[0].id)
             print(tweet[0].id)  # reply to most recent tweet
             oldtweet = tweet
             time.sleep(60)
