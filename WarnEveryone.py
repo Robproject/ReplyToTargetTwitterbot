43092c616c2c2627603f45769f0246b25dbfc293
Robert <robirtduncan@gmail.com>
2021-04-11 19:50:02 -0700

    fixed formatting

--- WarnEveryone.py
+++ WarnEveryone.py
@@ -1,33 +1,37 @@
-
 # Twitterbot/bots/followfollowers.py
 
 
 from config import create_api
 import time
+
 api = create_api()
+
 
 def warn_everyone(screen_name):
     oldtweet = ""
-    HelpfulBotSays = " Disclaimer: This post not financial advice. Subsequent and potentially related market swings may be highly volatile. Independent due diligence is always recommended."
+    helpful_bot_says = "Disclaimer: This post not financial advice. Subsequent and potentially related market swings " \
+                       "may be highly volatile. Independent due diligence is always recommended. "
     while True:
-        tweet = api.user_timeline(screen_name=screen_name, count=1) #get most recent status/tweet
-        if tweet == oldtweet:   #if it's the same as the most recent tweet, skip
+        tweet = api.user_timeline(screen_name=screen_name, count=1)  # get most recent status/tweet
+        if tweet == oldtweet:  # if it's the same as the most recent tweet, skip
             time.sleep(60)
             print(tweet[0].id)
-        elif HelpfulBotSays == api.user_timeline(screen_name="NiceHelpfulBot1", count=1)[0].text: #see bot's most recent tweet text, if its the same then sleep
+        elif helpful_bot_says == api.user_timeline(screen_name="NiceHelpfulBot1", count=1)[0].text:
+            # see bot's most recent tweet text, if its the same then sleep
             time.sleep(60)
             print(tweet[0].id)
             oldtweet = tweet
         else:
-            api.update_status("@" + screen_name + HelpfulBotSays, tweet[0].id)
+            api.update_status("@" + screen_name + helpful_bot_says, tweet[0].id)
             print(tweet[0].id)  # reply to most recent tweet
             oldtweet = tweet
             time.sleep(60)
+
+
 def main():
-
     while True:
-
         warn_everyone("elonmusk")
+
 
 if __name__ == "__main__":
     main()
